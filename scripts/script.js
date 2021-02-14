e3fbb14cc9bd1807b3a1288b6b21d62afe653569
kyeling <kyeling@users.noreply.github.com>
2021-02-13 22:31:27 -0800

    missing ) in script.js:6 (resolved issue: not loading items at all)

--- scripts/script.js
+++ scripts/script.js
@@ -3,22 +3,19 @@
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
   const request = async() => {
-    if(localStorage.getItem('items' === null)){
+    if(localStorage.getItem('items') == null){
       const res = await fetch('https://fakestoreapi.com/products')
         .then(response => response.json())
         .then(json => localStorage.setItem('items', JSON.stringify(json)));
     }
     populatePage();
   }
-
   request();
 });
 
 function populatePage() {
-  console.log("why is this not working");
   let container = document.getElementById('product-list');
   let data = JSON.parse(localStorage.getItem('items'));
-  console.log(localStorage.getItem('items'));
   for(i in data) {
     container.appendChild(new ProductItem(data[i].id, data[i].title, data[i].price, data[i].image));
   }
